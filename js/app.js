ca711563bf7bf0bd40355e459868d17efb8c8799
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-20 15:08:30 +0100

    EDIT+STYLE modificato carosello

--- js/app.js
+++ js/app.js
@@ -5,27 +5,27 @@ const app = Vue.createApp({
         images: [
           {
             title: "Daenerys Targaryen",
-            text: "blabla",
+            text: "Io sono Daenerys, Nata dalla Tempesta, della Casa Targaryen, dello antico sangue di Valyria e vi dico che sono io la figlia del Drago. Vi do la mia parola che coloro che vi faranno del male moriranno tra urla di tormento",
             image: "./img/image1.jpg"
           },
           {
             title: "Jon Snow",
-            text: "blabla",
+            text: "Tu appartieni al nord...il vero nord.",
             image: "./img/image2.jpg"
           },
           {
             title: "Cersei Lannister",
-            text: "blabla",
+            text: "Al gioco del trono o si vince o si muore. Non c'è una terza possibilità.",
             image: "./img/image3.jpg"
           },
           {
             title: "Tyrion Lannister",
-            text: "blabla",
+            text: "Sono colpevole di un crimine più mostruoso: essere un nano. La mia colpa è questa.",
             image: "./img/image4.jpg"
           },
           {
             title: "The Night King",
-            text: "blabla",
+            text: "Il vero nemico non aspetterà la fine delle bufere. Lui porta le bufere.",
             image: "./img/image5.jpg"
           }
 
